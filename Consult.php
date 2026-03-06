8549388ce9ffb11b705139d6f8128cebb1602b89
user.name "Jyotirmay Biswas <jyotirmaybiswas2419@gmail.com>
2026-03-06 22:47:21 +0530

    gg

--- Consult.php
+++ Consult.php
@@ -1,9 +1,20 @@
 <?php
-$isProduction = getenv('VERCEL') == "1" || (!file_exists(__DIR__ . '/node_modules/.vite') && !isset($_SERVER['HTTP_X_VITE_DEV']));
 $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') ? "https" : "http";
 $host = $_SERVER['HTTP_HOST'];
 $dev_server = "$protocol://" . explode(':', $host)[0] . ":5173";
 $base = rtrim(dirname($_SERVER['SCRIPT_NAME']), '/\\');
+
+// Check if Vite dev server is running locally
+$vite_ip = explode(':', $host)[0];
+$vite_running = false;
+if (getenv('VERCEL') != "1") {
+    $conn = @fsockopen($vite_ip, 5173, $errno, $errstr, 0.1);
+    if ($conn) {
+        $vite_running = true;
+        fclose($conn);
+    }
+}
+$isProduction = getenv('VERCEL') == "1" || !$vite_running;
 
 $success = false;
 $errors = [];
