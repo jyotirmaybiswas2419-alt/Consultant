019f452ee51eccfee80947d23f7638f9b763d810
user.name "Jyotirmay Biswas <jyotirmaybiswas2419@gmail.com>
2026-03-11 14:22:33 +0530

    service done

--- services.php
+++ services.php
@@ -31,11 +31,11 @@ $base = rtrim(dirname($_SERVER['SCRIPT_NAME']), '/\\');
         </section>
 
         <section class="Stats-section pt-10">
-            <div class="container md:p-15 px-5 py-5">
+            <div class="container md:p-15 px-5 py-5 text-center">
                 <div class="row">
                     <div
                         class="experience-content flex flex-col lg:flex-row justify-between lg:gap-50 md:gap-15 gap-3 text-[#31110F]">
-                        <div class="experience-stat">
+                        <div class="experience-stat ">
                             <p class="experience-number count-up" data-target="30" data-suffix="+">0+</p>
                             <p class="experience-text reveal-text">Years of Experience</p>
                         </div>
@@ -51,6 +51,80 @@ $base = rtrim(dirname($_SERVER['SCRIPT_NAME']), '/\\');
                 </div>
             </div>
         </section>
+
+        <section class="Our-services-section mt-10 bg-[#0D1F08] text-white">
+            <div class="container md:p-15 px-5 py-15">
+                <div class="row">
+                    <p class="lg:text-[32px] md:text-[28px] text-[24px]">Navigating Complex
+                        Legal Landscapes? </p>
+                    <p class="lg:text-[32px] md:text-[28px] text-[24px]">We Can Help.</p>
+                </div>
+                <div class="row pt-10 lg:mt-30">
+                    <div class="">
+                        <div class="our-serives mt-20 grid md:grid-cols-2 lg:block gap-5">
+                            <div class="Business-Corporate-Law flex xl:flex-row flex-col gap-5 md border-t py-10">
+                                <p class="serives-title text-[22px] font-medium w-full ">Business and Corporate Law</p>
+                                <p class="serives-description text-[18px] lg:w-[60%] ">Business formation (LLCs,
+                                    corporations,
+                                    partnerships)<br>
+                                    Contract drafting and review<br>
+                                    Mergers and acquisitions<br>
+                                    Intellectual property protection<br>
+                                    Employment law and workplace policies<br>
+                                    Corporate governance<br>
+                                    Shareholder and partnership disputes.</p>
+                            </div>
+                            <div
+                                class="Litigation-Dispute-Resolution flex xl:flex-row flex-col gap-5 md border-t py-10">
+                                <p class="serives-title text-[22px] font-medium w-full ">Litigation and Dispute
+                                    Resolution</p>
+                                <p class="serives-description text-[18px] lg:w-[60%] ">Civil litigation</br>
+                                    Commercial litigation</br>
+                                    Arbitration and mediation</br>
+                                    Class action lawsuits</br>
+                                    Personal injury claims</br>
+                                    Product liability cases.</p>
+                            </div>
+                            <div class="Family-Law flex xl:flex-row flex-col gap-5 md border-t py-10">
+                                <p class="serives-title text-[22px] font-medium w-full ">Family Law</p>
+                                <p class="serives-description text-[18px] lg:w-[60%] ">Divorce and separation</br>
+                                    Child custody and support</br>
+                                    Prenuptial and postnuptial agreements</br>
+                                    Adoption and surrogacy</br>
+                                    Domestic violence protection</p>
+                            </div>
+                            <div class="Criminal-Defense flex xl:flex-row flex-col gap-5 md border-t py-10">
+                                <p class="serives-title text-[22px] font-medium w-full ">Criminal Defense</p>
+                                <p class="serives-description text-[18px] lg:w-[60%] ">DUI and traffic offenses</br>
+                                    White-collar crime defense</br>
+                                    Drug charges</br>
+                                    Assault and battery cases</br>
+                                    Expungements and record sealing</p>
+                            </div>
+                            <div class="Real-Estate-Law flex xl:flex-row flex-col gap-5 md border-t py-10">
+                                <p class="serives-title text-[22px] font-medium w-full ">Real Estate Law</p>
+                                <p class="serives-description text-[18px] lg:w-[60%] ">Property transactions (buying,
+                                    selling, leasing)</br>
+                                    Landlord-tenant disputes</br>
+                                    Zoning and land use</br>
+                                    Construction law</br>
+                                    Real estate development</p>
+                            </div>
+                            <div class="Immigration-Law flex xl:flex-row flex-col gap-5 md border-t py-10">
+                                <p class="serives-title text-[22px] font-medium w-full ">Immigration Law</p>
+                                <p class="serives-description text-[18px] lg:w-[60%] ">Visa applications</br>
+                                    Green cards and citizenship</br>
+                                    Deportation defense</br>
+                                    Employment-based immigration</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="service-section-breaker">
+        </section>
+
         <section class="py-20">
             <div class="container  md:p-15 px-5 py-5">
                 <h1 class="text-[48px] font-light text-[#0d1f08] mb-6">Our Services</h1>
